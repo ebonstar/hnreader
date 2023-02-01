f2a1f42f4e4280a2593204800d3e2cf6bc33c47c
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-02-01 05:42:20 +0000

    add workflow to deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
