ebb96a0a2f70cb6ddb357d6cb6fc530a9250c27c
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-02-01 01:12:40 +0000

    add testing-library for component tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,11 @@
 // msw does not support node 18+ yet, see https://github.com/mswjs/msw/issues/1388
 // remove whatwg-fetch and the node environment variable in package.json when support is added
 import "whatwg-fetch";
-import { beforeAll, afterEach, afterAll } from "vitest";
+import matchers from "@testing-library/jest-dom/matchers";
+import { beforeAll, afterEach, afterAll, expect } from "vitest";
 import { server } from "mocks/server";
+
+expect.extend(matchers);
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => server.resetHandlers());
